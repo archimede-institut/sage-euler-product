da7b6f9309912f3a2481d0ef78b97b19a96899ae
Olivier Ramaré <89833526+ramare-olivier@users.noreply.github.com>
2024-04-01 20:09:35 +0200

    Update utils_euler_product.py

--- euler_product/utils_euler_product.py
+++ euler_product/utils_euler_product.py
@@ -298,10 +298,10 @@ class ComponentStructure():
       This list is the same as the list of necessary values of ``m̀``.
     - ``get_CA_Km``: a method used for ``get_vs``. The coefficient C(A,K,m) are a sum on a variable t of a function of the value computed by ``getr_A_K_t``.
       The lattice class A in given by its index ``ind_A`` in ``the_Class_tuple``, the subgroup K is given by its index ``ind_K`` in ``the_SG_tuple``.
-      The function ``get_CA_Km``answers a dictionary which to every ``(ind_A, ind_K, m)`` associates this value.
+      The function ``get_CA_Km`` answers a dictionary which to every ``(ind_A, ind_K, m)`` associates this value.
     - ``get_CA_Km_F_sur_H``: a method used for ``get_euler_products`. The coefficient C(A,K,m, F/H) are a sum on a variable t of s(F/H, m/t) times a function of the value computed by ``getr_A_K_t``.
       The lattice class A in given by its index ``ind_A`` in ``the_Class_tuple``, the subgroup K is given by its index ``ind_K`` in ``the_SG_tuple``.
-      The function ``get_CA_Km_F_sur_H``answers a dictionary which to every ``(ind_A, ind_K, m)`` associates this value.
+      The function ``get_CA_Km_F_sur_H`` answers a dictionary which to every ``(ind_A, ind_K, m)`` associates this value.
       When ``F == 1-X``and ``H == 1``, the output of ``get_CA_Km_F_sur_H`` is the same as the one of ``get_CA_Km``.
     - ``get_L_values``: a method used only for ``get_gamma``.
     - ``get_gamma``: a method.
